9544225c581528328a509f212e22e2a9e3e952d9
codedm24 <mithun.adroit@gmail.com>
2025-11-24 13:31:02 +0530

    Commented AI Engine Status Indicator

--- src/SmartDataAnalyst.Frontend/src/components/layout/MasterLayout.tsx
+++ src/SmartDataAnalyst.Frontend/src/components/layout/MasterLayout.tsx
@@ -3,16 +3,14 @@ import { Container, Row, Col } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
-import StatusIndicator from "./StatusIndicator";
+// import StatusIndicator from "./StatusIndicator";
 
 const MasterLayout: React.FC = () => {
   return (
     <div className="d-flex flex-column vh=-100">
       <div className="d-flex justify-content-between align-items-center bg-dark p-2">
         <Navbar />
-        <div className="me-3">
-          <StatusIndicator />
-        </div>
+        <div className="me-3">{/* <StatusIndicator /> */}</div>
       </div>
       <Container fluid className="flex-grow-1">
         <Row className="h-100">
