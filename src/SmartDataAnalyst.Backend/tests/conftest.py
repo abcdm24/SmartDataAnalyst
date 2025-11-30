37f3c8e0ba6465b6dafa728b43c410cd579e518e
codedm24 <mithun.adroit@gmail.com>
2025-11-30 20:21:47 +0530

    New test conf function for mocking llm call

--- src/SmartDataAnalyst.Backend/tests/conftest.py
+++ src/SmartDataAnalyst.Backend/tests/conftest.py
@@ -3,7 +3,7 @@ import os
 import pathlib
 import pytest
 import asyncio
-
+from unittest.mock import AsyncMock, patch
 
 PROJECT_ROOT = pathlib.Path(__file__).resolve().parents[1]
 
@@ -16,3 +16,13 @@ def event_loop():
     loop = asyncio.get_event_loop()
     yield loop
     loop.close()
+
+@pytest.fixture(autouse=True)
+def mock_llm():
+    """
+    Automatically mock llm_client.ask_llm for every test.
+    Prevents readl Azure/OpenAI calls.
+    """
+
+    with patch("src.SmartDataAnalyst.Backend.core.llm_client.ask_llm", AsyncMock(return_value='{"action":"done"}')):
+        yield
