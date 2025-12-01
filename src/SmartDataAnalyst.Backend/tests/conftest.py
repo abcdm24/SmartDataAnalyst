b35f66392f2bc16b9a92ad040a7ec41821d74392
codedm24 <mithun.adroit@gmail.com>
2025-12-01 11:45:23 +0530

    commented global mock llm function

--- src/SmartDataAnalyst.Backend/tests/conftest.py
+++ src/SmartDataAnalyst.Backend/tests/conftest.py
@@ -17,12 +17,12 @@ def event_loop():
     yield loop
     loop.close()
 
-@pytest.fixture(autouse=True)
-async def mock_llm():
-    """
-    Automatically mock llm_client.ask_llm for every test.
-    Prevents readl Azure/OpenAI calls.
-    """
-    print("Mocking ask_llm")
-    with patch("core.llm_client.ask_llm", AsyncMock(return_value='{"action":"done"}')):
-        yield
+# @pytest.fixture(autouse=True)
+# async def mock_llm():
+#     """
+#     Automatically mock llm_client.ask_llm for every test.
+#     Prevents readl Azure/OpenAI calls.
+#     """
+#     print("Mocking ask_llm")
+#     with patch("core.llm_client.ask_llm", AsyncMock(return_value='{"action":"done"}')):
+#         yield
